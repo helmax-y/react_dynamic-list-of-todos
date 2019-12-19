41b4df3117f9f0fb262dee10d2979bb69f6f1b76
helmax-y <max_rad@ukr.net>
2019-12-19 18:20:48 +0200

    Updates

---
+++ precache-manifest.17feb449cb45f72c06cc39391179f56a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae51864286915e1307ffde89e473d54",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "4c3b7fb8a3e20dd502ab",
+    "url": "/react_dynamic-list-of-todos/static/css/main.8e626f93.chunk.css"
+  },
+  {
+    "revision": "acbcc2aa7f41b7987eaf",
+    "url": "/react_dynamic-list-of-todos/static/js/2.9eba7f7b.chunk.js"
+  },
+  {
+    "revision": "4c3b7fb8a3e20dd502ab",
+    "url": "/react_dynamic-list-of-todos/static/js/main.ce598b9a.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
